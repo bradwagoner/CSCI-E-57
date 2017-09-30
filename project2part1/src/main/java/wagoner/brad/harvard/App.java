d0c080a4c796791a0348a972948e30aa4599b8ed
Brad Wagoner <bradley.wagoner@csra.com>
2017-09-30 08:59:13 -0500

    Created project from maven-archetype-quickstart archetype.

---
+++ project2part1/src/main/java/wagoner/brad/harvard/App.java
@@ -0,0 +1,13 @@
+package wagoner.brad.harvard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
