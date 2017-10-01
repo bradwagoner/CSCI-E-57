f1657480cf6f7ce9cbecc3a0175b451a0e3657f7
Brad Wagoner <bradley.wagoner@csra.com>
2017-10-01 06:02:32 -0500

    Project Setup: Setup a .gitignore for target and IDE file/folders. Added Log4j and configuration props file. Added build plugin to package executable jar. Added (currently unused jdbc.properties) and empty schema/test data sql files. Added skeleton app context xml.

--- project2part1/src/main/java/wagoner/brad/harvard/App.java
+++ project2part1/src/main/java/wagoner/brad/harvard/App.java
@@ -1,13 +1,20 @@
 package wagoner.brad.harvard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
+ * CSCI E-57: Term Project Part II
+ * Assignment-2.1
  *
+ * This project is runnable via the following commands:
+ * mvn clean install;
+ * java -jar ./target/project2part1-0.0-SNAPSHOT.jar;
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.debug("Executing App.main()");
     }
 }
